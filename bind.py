d178492e6c9b0713b83deae0cc63de4600829399
Mhorian <137920503+Mhorian@users.noreply.github.com>
2023-06-27 20:12:15 +0330

    Create bind.py

---
+++ bind.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Simple Text Editor")
+
+window.rowconfigure(0, minsize=800, weight=1)
+window.columnconfigure(1, minsize=800, weight=1)
+
+txt_edit = tk.Text(window)
+frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+btn_open = tk.Button(frm_buttons, text="Open")
+btn_save = tk.Button(frm_buttons, text="Save As...")
